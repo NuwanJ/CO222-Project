22c2dacb13881839075d6174adea9a0070283f04
NuwanJ <nuwanjaliyagoda@gmail.com>
2018-06-11 12:35:39 +0530

    msg

--- code/code.c
+++ code/code.c
@@ -5,9 +5,28 @@
 
 int main(){
 
+	char buffer[100][100] = {};
+	int mode = 0, i=0, j=0;
+	int puzCount = 0;
+	int wordlCount = 0;
 
 
+	while(mode!=2){
 
+		scanf("%c", &buffer[i][j]);
+
+		if(buffer[i][j]==10){	// Newline
+			if((buffer[i][0]==10)){
+				mode++;
+				printf(">> mode=%d\n", mode);
+			}
+			i++;j=0;
+
+		}else{
+			j++;
+		}
+	}
+	
 
 	return 0;
 }
