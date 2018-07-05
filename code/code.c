ff27bc6a6778196ef55281ca3ace2921bb52319b
NuwanJ <nuwanjaliyagoda@gmail.com>
2018-07-05 20:28:40 +0530

    #1

--- code/code.c
+++ code/code.c
@@ -6,12 +6,14 @@
 
 #define MAX_CHARS 50
 
-int debug=0;
+int worker(int rows, int cols, char **buffer, char *word, int len);
+
+int debug=1;
 int readFromFile = 0;
 
 int main(){
 	FILE *fp;
-	char buffer[MAX_CHARS][50] = {};			// Assumption
+	char buffer[MAX_CHARS][30] = {};			// Assumption
 	char word[MAX_CHARS];						// Assumption
 	int mode = 0, i=0, j=0, w1=0;
 	int rows = 0, cols=0;
@@ -49,7 +51,7 @@ int main(){
 					len = strlen(buffer[i]);
 					cols = (len>cols) ? len : cols;
 				}
-				if(mode==1) wordCount++;					// mode1=words
+				if(mode==1) wordCount++;					// mode1 = words
 			}
 			i++;j=0;
 
@@ -84,10 +86,11 @@ int main(){
 	wLen = (int *)malloc(wordCount*sizeof(int));
 
 	for(i=0;i<wordCount;i++){
-		 *(w+i) = (char *)malloc(strlen(buffer[rows + 1 + i])*sizeof(char));
+		 *(w+i) = (char *)malloc((strlen(buffer[rows + 1 + i]) + 1)*sizeof(char));
 	}
 
 	// Adding words to an array according to increasing length
+
 	int index=0;
 	for(int j=0;j<MAX_CHARS;j++){
 		for(i=0;i<wordCount;i++){
@@ -125,7 +128,7 @@ int main(){
 				// now found a #, lets look right for how many # points
 				wordCounter = 0;
 
-				while (p[j][i] == '#'){
+				while (j<rows && p[j][i] == '#'){
 					wordCounter++;
 					j++;
 				}
@@ -160,7 +163,10 @@ int main(){
 		strcpy(word, w[w1]);
 		if (debug) printf("\n%s (%d)> \n", word, len);
 
-		for(i=0;i<rows;i++){							// <-- each row
+
+		worker(rows, cols, p, word, len);
+
+		/*for(i=0;i<rows;i++){							// <-- each row
 			for(int j=0;j<=cols;j++){				// <-- each col
 				
 				if (buffer[i][j] == '#' || buffer[i][j]== word[0]){
@@ -175,6 +181,7 @@ int main(){
 
 						if(!(buffer[i][j2] == '#' || buffer[i][j2] == word[j2-j])){
 							possible = 0;
+							break;				// Stop further checking
 						}
 						if(debug) printf(" (%c)", buffer[i][j2]);
 						j2++;
@@ -200,6 +207,7 @@ int main(){
 
 						if(!(buffer[i2][j] == '#' || buffer[i2][j] == word[i2-i])){
 							possible = 0;
+							break;				// Stop further checking
 						}
 						if(debug) printf(" (%c)", buffer[i2][j]);
 						i2++;
@@ -218,16 +226,157 @@ int main(){
 
 				}
 			}
-		}
+		}*/
+
 	}
 
 	if(debug) printf("\n");
 
 	//printf("\n\n\nPuzzle:\n");
 	for(i=0;i<rows;i++){
-		printf("%s\n", buffer[i]);
+		printf("%s\n", p[i]);
 	}
 
 	return 0;
 }
 
+
+
+
+
+int worker(int rows, int cols, char **buffer, char *word, int len){
+
+	int i,j, used=0;
+
+	for(i=0;i<rows;i++){							// <-- each row
+		for(int j=0;j<=cols;j++){				// <-- each col
+			if (buffer[i][j] == '#' || buffer[i][j]== word[0]){
+
+				if (debug) printf("  %d %d right >> ", i, j);
+
+				// try to fit it on horizontal axis
+				int j2=j,possible=1; 
+
+				while (j2<(len+j) && possible==1){ 
+					// try to increase and check word can be fit or not
+
+					if(!(buffer[i][j2] == '#' || buffer[i][j2] == word[j2-j])){
+						possible = 0;
+						break;				// Stop further checking
+					}
+					if(debug) printf(" (%c)", buffer[i][j2]);
+					j2++;
+				}
+
+				if (possible==1 && (buffer[i][j+len] == '*' || buffer[i][j+len] == 0) && (j==0 || buffer[i][j-1]!='#')){
+					if(debug) printf(" Possible Situation\n");
+					
+					for (int j2=0;j2<len;j2++){
+						buffer[i][j + j2] = word[j2];
+					}
+					used = 1;
+					break;
+				}
+
+				if (debug) printf("\n  %d %d down  >> ", i, j);
+
+				// Try to fix it on verticle axis
+				int i2=i; possible = 1;
+
+				while (i2<(len+i) && possible==1){
+					// try to increase and check work can be fit or not
+
+					if(!(buffer[i2][j] == '#' || buffer[i2][j] == word[i2-i])){
+						possible = 0;
+						break;				// Stop further checking
+					}
+					if(debug) printf(" (%c)", buffer[i2][j]);
+					i2++;
+				}
+
+				if (possible==1 && (buffer[i+len][j] == '*' || buffer[i+len][j] == 0) && (i==0 || buffer[i-1][j]!='#')){
+					if(debug) printf(" Possible\n");
+					
+					for (int i2=0;i2<len;i2++){
+						buffer[i + i2][j] = word[i2];
+					}
+					used = 1;
+					break;
+				}
+				if(debug) printf("\n");
+			}
+		}
+	}
+
+	return 0;
+}
+
+
+
+/*
+int worker(int rows, int cols, char **buffer, char *word, int len){
+
+	int i,j, used=0;
+
+	for(i=0;i<rows;i++){							// <-- each row
+		for(int j=0;j<=cols;j++){				// <-- each col
+			if (buffer[i][j] == '#' || buffer[i][j]== word[0]){
+
+				if (debug) printf("  %d %d right >> ", i, j);
+
+				// try to fit it on horizontal axis
+				int j2=j,possible=1; 
+
+				while (j2<(len+j) && possible==1){ 
+					// try to increase and check word can be fit or not
+
+					if(!(buffer[i][j2] == '#' || buffer[i][j2] == word[j2-j])){
+						possible = 0;
+						break;				// Stop further checking
+					}
+					if(debug) printf(" (%c)", buffer[i][j2]);
+					j2++;
+				}
+
+				if (possible==1 && (buffer[i][j+len] == '*' || buffer[i][j+len] == 0) && (j==0 || buffer[i][j-1]!='#')){
+					if(debug) printf(" Possible Situation\n");
+					
+					for (int j2=0;j2<len;j2++){
+						buffer[i][j + j2] = word[j2];
+					}
+					used = 1;
+					break;
+				}
+
+				if (debug) printf("\n  %d %d down  >> ", i, j);
+
+				// Try to fix it on verticle axis
+				int i2=i; possible = 1;
+
+				while (i2<(len+i) && possible==1){
+					// try to increase and check work can be fit or not
+
+					if(!(buffer[i2][j] == '#' || buffer[i2][j] == word[i2-i])){
+						possible = 0;
+						break;				// Stop further checking
+					}
+					if(debug) printf(" (%c)", buffer[i2][j]);
+					i2++;
+				}
+
+				if (possible==1 && (buffer[i+len][j] == '*' || buffer[i+len][j] == 0) && (i==0 || buffer[i-1][j]!='#')){
+					if(debug) printf(" Possible\n");
+					
+					for (int i2=0;i2<len;i2++){
+						buffer[i + i2][j] = word[i2];
+					}
+					used = 1;
+					break;
+				}
+				if(debug) printf("\n");
+			}
+		}
+	}
+
+	return 0;
+}*/
